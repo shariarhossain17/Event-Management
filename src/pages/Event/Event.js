9aa3dbb95b0ef510b88993668b8a1341100799ef
shariarhossain23e <shariarhossain23@gamil.comm>
2022-09-28 18:46:49 +0600

    show inputt data on event field

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { AiOutlineCalendar } from "react-icons/ai";
 import { useSelector } from "react-redux";
-
+import img from '../../assests/Birthday cake.png';
 import "../Event/Event.css";
 
 const Event = () => {
   const event = useSelector((state) => state.counter);
-  console.log(event);
   return (
-    <div>
-      <h1>Hello</h1>
+   <section className="event-section">
+     <div className="event">
+      <div>
+        <h1>{event.event}</h1>
+        <h4>Hosted by {event.host}</h4>
+        <div className="date">
+            <div>
+                <AiOutlineCalendar/>
+            </div>
+            <div>
+                <h6>{event.start}</h6>
+                <h6>to {event.end}</h6>
+            </div>
+        </div>
+      </div>
+      <div>
+        <img className="event-img" src={img} alt="" />
+      </div>
     </div>
+   </section>
   );
 };
 
