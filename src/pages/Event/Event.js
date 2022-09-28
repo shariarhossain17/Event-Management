902554a00147cb104486f19d4c89ad75e5d01e1f
shariarhossain23e <shariarhossain23@gamil.comm>
2022-09-28 20:34:31 +0600

    updated readme md aded live site link

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -1,32 +1,50 @@
 import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
+import { IoLocationOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
-import img from '../../assests/Birthday cake.png';
+import img from "../../assests/Birthday cake.png";
 import "../Event/Event.css";
 
 const Event = () => {
   const event = useSelector((state) => state.counter);
   return (
-   <section className="event-section">
-     <div className="event">
-      <div>
-        <h1>{event.event}</h1>
-        <h4>Hosted by {event.host}</h4>
-        <div className="date">
-            <div>
-                <AiOutlineCalendar/>
+    <section className="event-section">
+      <div className="event">
+        <div className="event-left">
+          <h1 className="text-event">{event.event}</h1>
+          <p>
+            {" "}
+            <span>
+              {" "}
+              <h4 className="host">Hosted by {event.host}</h4>
+            </span>
+          </p>
+          <div className="date">
+            <div className="icon-bg">
+              <AiOutlineCalendar className="calender-icon" />
             </div>
             <div>
-                <h6>{event.start}</h6>
-                <h6>to {event.end}</h6>
+              <p className="start text-color">
+                {event.start} to {event.end}
+              </p>
             </div>
+          </div>
+          <div className="date">
+            <div className="icon-bg">
+              <IoLocationOutline className="calender-icon" />
+            </div>
+            <div>
+              <p className="start text-color">
+                {event.location}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img className="event-img" src={img} alt="" />
         </div>
       </div>
-      <div>
-        <img className="event-img" src={img} alt="" />
-      </div>
-    </div>
-   </section>
+    </section>
   );
 };
 
